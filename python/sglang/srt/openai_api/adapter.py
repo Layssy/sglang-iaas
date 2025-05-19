9d58909b4deac69c532e76d2beea24f32a0ce8a6
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2025-05-19 09:58:59 +0800

    Fix request id error (#6401)

--- python/sglang/srt/openai_api/adapter.py
+++ python/sglang/srt/openai_api/adapter.py
@@ -1855,6 +1855,7 @@ def v1_embedding_request(all_requests, tokenizer_manager):
                 prompt_kwargs = {"text": generate_prompts, "image_data": images}
         else:
             prompt_kwargs = {"input_ids": prompt}
+        request_ids = all_requests[0].rid
     else:
         if isinstance(prompts[0], str) or isinstance(prompts[0][0], str):
             prompt_kwargs = {"text": prompts}
@@ -1867,7 +1868,7 @@ def v1_embedding_request(all_requests, tokenizer_manager):
             )
         else:
             prompt_kwargs = {"input_ids": prompts}
-    request_ids = [req.rid for req in all_requests]
+        request_ids = [req.rid for req in all_requests]
 
     adapted_request = EmbeddingReqInput(
         rid=request_ids,
