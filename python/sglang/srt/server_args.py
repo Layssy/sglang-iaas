58d39d8f68c700013019781c77f3a9fd79edf7c9
HAI <hixiao@gmail.com>
2025-05-20 22:52:41 -0700

    aiter attention-backend (default enabled on AMD/ROCm) (#6381)

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -982,6 +982,7 @@ class ServerArgs:
             "--attention-backend",
             type=str,
             choices=[
+                "aiter",
                 "flashinfer",
                 "triton",
                 "torch_native",
